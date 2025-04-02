31674b024be8a7d7fa139e02eda230460bebc844
Cuidandoelagua517 <dgil@bairesanalitica.com>
2025-04-02 03:24:38 -0300

    Create lopp-end.php

---
+++ templates/loop/lopp-end.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Product Loop End
+ *
+ * Este template reemplaza el original de WooCommerce para asegurar que nuestra cuadrícula de productos
+ * cierre correctamente.
+ */
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
+?>
+</ul>
+<!-- .productos-grid -->
